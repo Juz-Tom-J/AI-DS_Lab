15432072aaa7c52664576fd2dae95af31539cff4
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2022-10-27 11:42:19 +0530

    Code added

--- Sorting Content in a CSV file.py
+++ Sorting Content in a CSV file.py
@@ -1,5 +1,5 @@
 import pandas as pd
 mark_sort = pd.read_csv('std_marks.csv')
 print(mark_sort)
-print(f"\nSorting Data (Name):\n{file.sort_values(['Name'])}")
-print(f"\nSorting Data (Marks):\n{file.sort_values(['Marks'])}")
+print(f"\nSorting Data (Name):\n{mark_sort.sort_values(['Name'])}")
+print(f"\nSorting Data (Marks):\n{mark_sort.sort_values(['Marks'])}")
